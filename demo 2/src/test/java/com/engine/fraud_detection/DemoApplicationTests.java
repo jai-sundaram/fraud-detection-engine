497f117540b9c4d7111ab21ebe2a2ad4a0e09485
Jai Sundaram <je.sundaram@gmail.com>
2026-04-16 15:04:37 -0400

    created spring boot project

---
+++ demo 2/src/test/java/com/engine/fraud_detection/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fraud_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
